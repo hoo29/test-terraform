a401dcdfa9e52f1ef39e7afa1754c761ca065290
Huw McNamara <huwmcnamara@msn.com>
2023-07-29 14:03:32 +0100

    create in plan

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 resource "aws_sns_topic" "test" {
   name = "in-terraform-apply"
 }
+
+data "external" "test" {
+  program = [
+    "aws",
+    "sns",
+    "create-topic",
+    "--name",
+    "in-terraform-plan",
+    "--region",
+    "eu-west-1",
+    "--output",
+    "json"
+  ]
+}
